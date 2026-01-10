c77247ab9bd20d3df4e85032ccc9dde2b2bc00ec
abroraxatov1 <abroraxatov1@gmail.com>
2026-01-10 09:16:10 +0500

    Update print statement from 'Hello' to 'Goodbye'

--- ekinAI.py
+++ ekinAI.py
@@ -724,12 +724,13 @@ with st.expander("Kiritilgan qiymatlar"):
 # Copyright at the bottom with optional logo
 logo_url = "https://www.samdu.uz/new/images/SamDU%20logo%20full%201.png"  # Replace with your logo URL or leave empty
 if logo_url:
-    st.markdown(f'<div class="copyright"><img src="https://www.samdu.uz/new/images/SamDU%20logo%20full%201.png" alt="Logo"> <a href="https://www.samdu.uz/uz" class="samdu"> Samarqand davlat universiteti    </a> <a href="http://ai-lab.sampc.uz/" class="lab"> "Sun\'iy intellekt labaratoriyasi" </a> <span class="ishlab"> tomonidan ishlab chiqilgan. © 2025 </span> <span class="ekin2">Ekin</span> <span class="ai2"> AI</span>.</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="copyright"><img src="https://www.samdu.uz/new/images/SamDU%20logo%20full%201.png" alt="Logo"> <a href="https://www.samdu.uz/uz" class="samdu"> Samarqand davlat universiteti    </a> <a href="http://ai-lab.sampc.uz/" class="lab"> "Sun\'iy intellekt labaratoriyasi" </a> <span class="ishlab"> tomonidan ishlab chiqilgan. © 2026 </span> <span class="ekin2">Ekin</span> <span class="ai2"> AI</span>.</div>', unsafe_allow_html=True)
 else:
 
     st.markdown(f'<div class="copyright"> <a href="https://www.samdu.uz/uz" class="samdu"> Samarqand davlat universiteti  </a> <a href="http://ai-lab.sampc.uz/" class="lab"> "Sun\'iy intellekt labaratoriyasi" </a> <span class="ishlab"> tomonidan ishlab chiqilgan. © 2025 </span> <span class="ekin2">Ekin</span> <span class="ai2"> AI</span>.</div>', unsafe_allow_html=True)
 
 st.markdown(f'<div class="author">  Muallif: <a href="https://t.me/axatov_a" class="lab"> Axatov Abror </a> <span class="ekin2">Tel: </span> <span class="ai2"> +998(99)-590-52-65 </span></div>', unsafe_allow_html=True)
+
 
 
 
